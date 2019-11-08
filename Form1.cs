172e9467aa8043978331e1fafa7bd8576b6d7f87
sungtae92 <56426242+sungtae92@users.noreply.github.com>
2019-11-08 16:51:10 +0900

    Update Form1.cs
    //

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Text = "again!";
+            button1.Text = "again!"; // hi
         }
     }
 }
