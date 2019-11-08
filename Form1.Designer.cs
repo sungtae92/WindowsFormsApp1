6f11faf5f03e34ca78e08b4fb417fd32d0e1849c
sungtae92 <sungtae6502@gmail.com>
2019-11-08 16:28:14 +0900

    changed text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,12 +44,14 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button1.Location = new System.Drawing.Point(23, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "click";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
